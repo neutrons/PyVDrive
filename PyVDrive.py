b2163d2b0ea8be2591a88c3eebb8a986aa3bb30d
Wenduo Zhou <zhouw@ornl.gov>
2015-01-26 17:23:45 -0500

    Checkpointing to prototype on a reduction workflow.

--- PyVDrive.py
+++ PyVDrive.py
@@ -4,6 +4,9 @@
 # - User can write script on this class to reduce and analyze data;
 ################################################################################
 
+import vdrive
+import vdrive.VDProject as vp
+
 class PyVDrive:
     """ Class PyVDrive
     """
@@ -11,16 +14,31 @@ class PyVDrive:
         """ Init
         """
         self._projectDict = {}
-        self._name = ""
         
         return
         
         
-    def newProject(self, projname):
+    def newProject(self, projname, projtype):
         """ Add a new project
-        """ 
-        self._name = projname
+        """         
+        # check project type
+        if projtype != 'reduction' and projtype != 'fit':
+            raise NotImplementedError("Project type %s is not supported." % (projtype))
         
+        # check available to add this new project
+        if self._projectDict.has_key(projname):
+            raise NotImplementedError("Project with name %s has existed.  Unable to add a new project with sname name." 
+                % (projname) )        
+        
+        # new project
+        if projtype == 'reduction':
+           newproject = vp.ReductioProject(projname)
+        elif projtype == 'analysis':
+            newproject = vp.AnalysisProject(projname)
+        
+        # add project to dictionary
+        self._projectDict[projname] = newproject
+                        
         return False
         
         
@@ -31,3 +49,73 @@ class PyVDrive:
         
         
         return False
+        
+    def hasProject(self, projname):
+        """ 
+        """
+        return self._projectDict.has_key(projname)
+        
+        
+    def deleteProject(self, projname):
+        """ Delete an existing project
+        """
+        self._checkProjectNotExistence(projname)
+        raise NotImplementedError("Implement ASAP")
+        
+        return False
+        
+    def reduce(self, projname):
+        """ Reduce the data
+        """
+        self._checkProjectExistence(projname, "reduce powder diffraction")
+        
+        self._projectDict[projname].reduce()
+        
+        
+    def addData(self, projname, datafilename):
+        """ Add data
+        """
+        self._checkProjectExistence(projname, "add data set")
+        
+        self._projectDict[projname].addData(datafilename)
+        
+        return
+        
+        
+    def setCalibFile(self, projname, calfilename):
+        """ Set calibration file in reduction mode
+        """
+        self._checkProjectExistence(projname, "set calibration file")
+        
+        self._projectDict[projname].setCalibrationFile(calfilename)
+        
+        return
+        
+        
+    def setCharactFile(self, projname, charactfilename):
+        """ Set SNS characterization file name to reduction
+        """
+        self._checkProjectExistence(projname, "set characterization file")
+        
+        self._projectDict[projname].setCharacterFile(charactfilename)
+        
+        return
+        
+    def setReductionParameters(self, projname, reductionparamdict):
+        """ Set reduction parameters
+        """
+        self._checkProjectExistence(projname, "set characterization file")
+        
+        self._projectDict[projname].setParameters(reductionparamdict)
+        
+        return
+        
+        
+    def _checkProjectExistence(self, projname, operation):
+        """ Check wehtehr a project (name) does exist
+        """
+        if self._projectDict.has_key(projname) is False:
+            raise NotImplementedError("Project %s exists. Unable to proceed the operation %s." % (
+                projname, operation))
+        
+        return
--- vdrive/VDProject.py
+++ vdrive/VDProject.py
@@ -1,24 +1,67 @@
+import sys
+
+sys.path.append("/home/wzz/Mantid/Code/debug/bin/")
+
+import mantid
+import mantid.simpleapi as mtd
+
 class VDProject:
     """
     """
-    def __init__(self):
+    def __init__(self, projname):
         """
         """
+        self._name = projname
+        self._calibfilename = None
+        self._dataset = []
+        
+        return
+        
+    def addData(self, datafilename):
+        """ Add a new data file to project
+        """
+        self._dataset.append(datafilename)
         
         
         
 class ReductioProject(VDProject):
     """
     """
-    def __init__(self):
+    def __init__(self, projname):
         """
         """
+        VDProject.__init__(self, projname)
+        
+        
+    def setCalibrationFile(self, calibfilename):
+        """ Set the calibration file
+        """
+        self._calibfilename = calibfilename
+        
+    def setCharacterFile(self, characerfilename):
+        """ Set characterization file
+        """
+        self._characterfilename = characerfilename
+        
+        
+    def setParameters(self, paramdict):
+        """ Set parameters in addition to those necessary
+        """
+        if isinstance(paramdict, dict) is False:
+            raise NotImplementedError("setParameters is supposed to get a dictionary")
+            
+        self._paramDict = paramdict
+        
         return
         
+    def reduce(self):
+        """
+        """
+        mtd.SNSPowderReduction()
         
-    def 
+        return
         
-        
+ 
         
 class AnalysisProject(VDProject):
     """
