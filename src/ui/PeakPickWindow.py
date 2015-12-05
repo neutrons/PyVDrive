d8af813d823c95f0a74272ef9dd821bd007d1231
Wenduo Zhou <zhouw@ornl.gov>
2015-12-04 23:01:53 -0500

    Refs #30. Added some event handling methods.

--- src/ui/PeakPickWindow.py
+++ src/ui/PeakPickWindow.py
@@ -35,19 +35,31 @@ class PeakPickerWindow(QtGui.QMainWindow):
         self.ui.setupUi(self)
 
         # Define event handling methods
-        """
-        pushButton_addCurrentPeak
-        pushButton_addAllPeaks
-        pushButton_loadCalibFile
-        pushButton_readData
-        pushButton_return
-        pushButton_save
-        """
+        self.connect(self.ui.pushButton_addCurrentPeak, QtCore.SIGNAL('clicked()'),
+                     self.do_add_current_peak)
 
-        raise NotImplementedError('Work on UI')
+        self.connect(self.ui.pushButton_addAllPeaks, QtCore.SIGNAL('clicked()'),
+                     self.do_add_all_peaks)
+
+        self.connect(self.ui.pushButton_loadCalibFile, QtCore.SIGNAL('clicked()'),
+                     self.do_load_calibration_file)
+
+        self.connect(self.ui.pushButton_readData, QtCore.SIGNAL('clicked()'),
+                     self.do_load_data)
+
+        self.connect(self.ui.pushButton_return, QtCore.SIGNAL('clicked()'),
+                     self.do_quit)
+
+        self.connect(self.ui.pushButton_save, QtCore.SIGNAL('clicked()'),
+                     self.do_save_peaks)
 
         # Set up widgets
         self._init_widgets_setup()
+
+        # Define state variables
+        self._dataLoaded = False  # state flag that data is loaded
+        self._currDataFile = None  # name of the data file that is loaded for current session
+        self._myController = None  # Reference to controller class
 
         return
 
@@ -60,8 +72,103 @@ class PeakPickerWindow(QtGui.QMainWindow):
 
         return
 
+    def initialize(self, controller):
+        """
+        Purpose:
+        Requires:
+        Guarantees:
+        :param controller:
+        :return:
+        """
 
-def testmain(argv):
+    def do_add_current_peak(self):
+        """
+        Purpose:
+            add current selected peak to table
+        Requires:
+            Window has been set with parent controller
+            data has been loaded;
+            mouse is on canvas
+            GUI is in peak selection mode
+        Guarantees
+            the peak under the cursor is added to table
+
+        :return: None
+        """
+        return
+
+    def do_add_all_peaks(self):
+        """
+        Purpose:
+            add all peaks that can be found in the data or on the canvas to table
+        Requires:
+            Window has been set with parent controller
+            data has been loaded;
+            GUI is in peak selection mode;
+        Guarantees
+        :return:
+        """
+        # Check requirements
+        assert self._myController is not None
+
+        return
+
+    def do_load_calibration_file(self):
+        """
+        Purpose:
+            Load calibration file
+        Requires:
+            None
+        Guarantees:
+            Calibration file is loaded and parsed
+
+        :return:
+        """
+        # Check requirements
+        assert self._myController is not None
+
+        # Launch dialog box for calibration file name
+        cal_file_name = QtGui.QFileDialog.getOpenFileName(self, 'Calibration File')
+
+        # Load
+        self._myController.load_calibration_file(cal_file_name)
+
+        return
+
+    def do_load_data(self):
+        """
+        Purpose:
+            Load GSAS data
+        Requirements:
+            Controller has been set to this object
+        Requires:
+        Guarantees:
+        :return:
+        """
+        # Check requirements
+        assert self._myController is not None
+
+        return
+
+    def do_quit(self):
+        """
+        Purpose:
+        Requires:
+        Guarantees:
+        :return:
+        """
+        return
+
+    def do_save_peaks(self):
+        """
+        Purpose:
+        Requires:
+        Guarantees:
+        :return:
+        """
+        return
+
+def main(argv):
     """ Main method for testing purpose
     """
     parent = None
@@ -78,4 +185,4 @@ def testmain(argv):
     return
 
 if __name__ == "__main__":
-    testmain(sys.argv)
+    main(sys.argv)
