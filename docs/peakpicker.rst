618441a0093bc10af47be5dbd625a75e2e1913b2
Wenduo Zhou <zhouw@ornl.gov>
2015-12-14 07:31:59 -0500

    Refs #30. Checkpointing progress on design the gui.

--- docs/peakpicker.rst
+++ docs/peakpicker.rst
@@ -1,4 +1,44 @@
-= Peak Picker Window =
+Peak Picker Window
+------------------
+
+This is the design document for peak picker window
 
 
-== Business Model ==
+Business Model
+==============
+
+Typical workflow
+++++++++++++++++
+
+A typical workflow is composed of a couple of steps
+  1. User have PeakPickerWindow to load a GSAS data file;
+  2. PeakPickerWindow plots the GSAS on canvas;
+  3. User pushes the *Find Peaks* button so that PeakPickerWindow finds all peaks in automatic mode.
+     The peaks that are found will be added to the table workspace with fitted parameters;
+  4. User pushes the *Plot Fitted Peaks* button so that the fitted peaks are plot against the experimental data;
+  5. User deletes some peaks that are not well defined;
+  6. User add some peaks that are not found by automatic algorithm.
+
+
+Features of interactive plotting
+++++++++++++++++++++++++++++++++
+
+Peak-picker window provides user with interactive plotting to work with diffraction peaks.
+Here are some operations that will be supported.
+
+  - Set a vertical (indictor) line to indicate a peak;
+  - Highlight a peak or a region of the diffraction pattern and undo the highlighting operation;
+  - Plot the model of a peak (i.e., fitted peak) and the difference to experimental data;
+  - Select the peak range and/or peak width.
+
+
+Technical Model
+===============
+
+This section introduces how to map the user required features to program.
+
+DiffracitonPlotView
++++++++++++++++++++
+
+DiffractionPlotView: 
+   It is an extension to MplCanvas such that it provides users with interactive plot
--- src/ui/peakparametertable.py
+++ src/ui/peakparametertable.py
@@ -1,17 +1,114 @@
 __author__ = 'wzz'
 
-import gui.ndav_widgets.NTableWidget
+import gui.ndav_widgets.NTableWidget as NT
 
 
-# TODO/NOW - #30
 class PeakParameterTable(gui.ndav_widgets.NTableWidget):
     """
-
+    A customized table to hold diffraction peaks with the parameters
     """
-    def __init__(self, parent):
-        """
+    PeakTableSetup = [('Index', 'int'),
+                      ('Centre', 'float'),
+                      ('Min D', 'float'),
+                      ('Max D', 'float'),
+                      ('Select', 'checkbox')]
 
+    def __init__(self, parent):
+        """ Initialization
+        Purpose:
+            Initialize the table objet
+        Requirements:
+            None
+        Guarantees:
+            An object is made
         :param parent:
         :return:
         """
+        NT.NTableWidget.__init__(self, parent)
 
+        return
+
+    def append_peak(self, centre, xmin, xmax):
+        """ Append a peak to the table
+        Purpose:
+            Append a new peak to the table
+        Requirements:
+            Peak centre must be in d-spacing.  And it is within the peak range
+        Guarantees:
+            A row is append
+        :param centre:
+        :param xmin: left boundary of peak range
+        :param xmax: right boundary of peak range
+        :return:
+        """
+        # Check requirements
+        assert isinstance(centre, float)
+        assert isinstance(xmin, float)
+        assert isinstance(xmax, float)
+        assert xmin < centre < xmax
+
+        # Get new index
+        new_index = self.rowCount()
+        self.append_row([new_index, centre, xmin, xmax, False])
+
+        return
+
+    def delete_peak(self, peak_index):
+        """ Delete a peak from the table
+        Purpose:
+            Delete a peak from the table
+        Requirements:
+            Given peak's index must be in the range
+        Guarantees:
+            The specified peak will be deleted from the table
+        :param peak_index:
+        :return:
+        """
+        # Check requirements
+        assert isinstance(peak_index, int)
+        assert 0 <= peak_index < self.rowCount(), 'Index of peak %d is out of boundary' % peak_index
+
+        # Remove peak
+        self.remove_row(peak_index)
+
+        # Update the peak index of each peak behind
+        for i_row in xrange(peak_index, self.rowCount()):
+            this_index = self.get_cell_value(i_row, 0)
+            self.update_cell_value(i_row, 0, this_index-1)
+        # END-FOR(i_row)
+
+        return
+
+    def get_peak(self, peak_index):
+        """ Get the peak's information from the table
+        Purpose:
+            Get the peak's information including centre, and range
+        Requirements:
+            Specified peak index must be in range
+        Guarantees:
+            Return a dictionary containing peak centre in d-dpacing and its range
+        :param peak_index:
+        :return:
+        """
+        # Check requirements
+        assert isinstance(peak_index, int)
+        assert 0 <= peak_index < self.rowCount(), 'Index of peak %d is out of boundary' % peak_index
+
+        # Get information
+        peak_dict = {'Centre': self.get_cell_value(peak_index, 1),
+                     'xmin': self.get_cell_value(peak_index, 2),
+                     'xmax': self.get_cell_value(peak_index, 3)}
+
+        return peak_dict
+
+    def setup(self):
+        """
+        Init setup
+        :return:
+        """
+        self.init_setup(PeakParameterTable)
+
+        # Set up column width
+        self.setColumnWidth(0, 20)
+
+        return
