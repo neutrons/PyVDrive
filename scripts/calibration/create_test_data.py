b23e7776b6b047404c89f799b2b25b9a601838a1
Wenduo Zhou <zhouw@ornl.gov>
2018-03-28 01:10:11 -0400

    Refs #99. Improved the cross-correlation script.

--- scripts/calibration/create_test_data.py
+++ scripts/calibration/create_test_data.py
@@ -1,12 +1,79 @@
-Load(Filename='/SNS/VULCAN/shared/PyVDrive-Dev/Calibration/VULCAN_150178.nxs.h5', OutputWorkspace='full_diamond', LoaderName='LoadEventNexus', LoaderVersion='1')
-ConvertUnits(InputWorkspace='full_diamond', OutputWorkspace='full_diamond', Target='dSpacing')
-Rebin(InputWorkspace='full_diamond', OutputWorkspace='full_diamond', Params='0.3,-0.0003,3')
-ConvertToMatrixWorkspace(InputWorkspace='full_diamond', OutputWorkspace='full_diamond')
-SaveNexusProcessed(InputWorkspace='full_diamond', Filename='/SNS/users/wzz/Projects/VULCAN/nED_Calibration/Diamond_NeXus/VULCAN_150178_HighResolution_Diamond.nxs')
-CropWorkspace(InputWorkspace='full_diamond', OutputWorkspace='west_bank', XMin='0.80000000000000004', XMax='1.5', EndWorkspaceIndex='3234')
-SaveNexusProcessed(InputWorkspace='west_bank', Filename='/SNS/users/wzz/Projects/VULCAN/nED_Calibration/Diamond_NeXus/VULCAN_150178_Diamond_WestBank.nxs', Title='Cropped High Resolution 31-Hour Diamond West Bank')
-Load(Filename='VULCAN_150178_Diamond_WestBank.nxs', OutputWorkspace='west')
-CropWorkspace(InputWorkspace='west', OutputWorkspace='west1', EndWorkspaceIndex=20)
-CropWorkspace(InputWorkspace='west', OutputWorkspace='west2', StartWorkspaceIndex=40, EndWorkspaceIndex=200)
-ConjoinWorkspaces(InputWorkspace1='west1', InputWorkspace2='west2')
+# Create a subset but representative MatrixWorkspace of diamond from VULCAN to test
+# cross-correlation
+from mantid.simpleapi import LoadNexusProcessed, CropWorkspace, ConjoinWorkspaces, SaveNexusProcessed
 
+
+def create_partial_diamond(src_nexus_name, target_nexus_name):
+    """
+
+    :param src_nexus_name:
+    :param target_nexus_name:
+    :return:
+    """
+    # load file
+    LoadNexusProcessed(Filname=src_nexus_name, OutputWorkspace='full_diamond')
+
+    # create workspace index list
+    ws_index_list = list()
+
+    # N sections on west bank
+    ws_index_list.append((0, 3))
+    ws_index_list.append((600, 603))
+    ws_index_list.append((1200, 1202))
+    ws_index_list.append((1800, 1802))
+    ws_index_list.append((2400, 2402))
+    ws_index_list.append((3200, 3202))
+
+    # N sections on east bank
+    base_index = 3234
+    ws_index_list.append((base_index+0,    base_index+3))
+    ws_index_list.append((base_index+600,  base_index+603))
+    ws_index_list.append((base_index+1200, base_index+1202))
+    ws_index_list.append((base_index+1800, base_index+1802))
+    ws_index_list.append((base_index+2400, base_index+2402))
+    ws_index_list.append((base_index+3200, base_index+3202))
+
+    # N sections on high angle bank
+    base_index = 6468
+    ws_index_list.append((base_index+0,    base_index+3))
+    ws_index_list.append((base_index+600,  base_index+603))
+    ws_index_list.append((base_index+1200, base_index+1202))
+    ws_index_list.append((base_index+1800, base_index+1802))
+    ws_index_list.append((base_index+2400, base_index+2402))
+    ws_index_list.append((base_index+3200, base_index+3202))
+
+    # Crop and conjoin
+    CropWorkspace(InputWorkspace='full_diamond', OutputWorkspace='partial_diamond',
+                  StartWorkspaceIndex=ws_index_list[0][0], EndWorkspaceIndex=ws_index_list[0][1])
+
+    for seq in range(1, len(ws_index_list)):
+        temp_name = 'temp_{0}'.format(seq)
+        # crop
+        CropWorkspace(InputWorkspace='full_diamond', OutputWorkspace=temp_name,
+                      StartWorkspaceIndex=ws_index_list[seq][0], EndWorkspaceIndex=ws_index_list[seq][1])
+        # conjoin
+        ConjoinWorkspaces(InputWorkspace1='partial_diamond', InputWorkspace2=temp_name)
+    # END-FOR (seq)
+
+    # save/output
+    SaveNexusProcessed(InputWorkspace='partial_diamond',
+                       Filename=target_nexus_name,
+                       Title='Partial 31 hour diamond run')
+
+    return
+
+
+def main():
+    """
+    main
+    :param argv:
+    :return:
+    """
+    diamond_nexus = \
+        '/SNS/users/wzz/Projects/VULCAN/nED_Calibration/Diamond_NeXus/VULCAN_150178_HighResolution_Diamond.nxs'
+
+    create_partial_diamond(diamond_nexus, 'vulcan_partial_diamond.nxs')
+
+
+if __name__ == '__main__':
+    main()
--- scripts/calibration/cross_correlation.py
+++ scripts/calibration/cross_correlation.py
@@ -1,8 +1,9 @@
 # script to do cross-correlation
 import os
-from mantid.kernel import AnalysisDataService as mtd
+from mantid.api import AnalysisDataService as mtd
 from mantid.simpleapi import CrossCorrelate, GetDetectorOffsets, SaveCalFile, ConvertDiffCal, SaveDiffCal
-from mantid.simpleapi import RenameWorspace, Plus
+from mantid.simpleapi import RenameWorkspace, Plus
+import bisect
 
 
 def cc_calibrate(ws_name, peak_position, peak_min, peak_max, ws_index_range, reference_ws_index, cc_number, max_offset,
@@ -37,8 +38,9 @@ def cc_calibrate(ws_name, peak_position, peak_min, peak_max, ws_index_range, ref
     print ('Reference spectra=%s' % reference_ws_index)
 
     # Cross correlate spectra using interval around peak at peakpos (d-Spacing)
+    cc_ws_name = 'cc_' + ws_name + '_' + index
     CrossCorrelate(InputWorkspace=ws_name,
-                   OutputWorkspace=ws_name+"cc"+index,
+                   OutputWorkspace=cc_ws_name,
                    ReferenceSpectra=reference_ws_index,
                    WorkspaceIndexMin=ws_index_range[0], WorkspaceIndexMax=ws_index_range[1],
                    XMin=peak_min, XMax=peak_max)
@@ -46,7 +48,7 @@ def cc_calibrate(ws_name, peak_position, peak_min, peak_max, ws_index_range, ref
     # Get offsets for pixels using interval around cross correlations center and peak at peakpos (d-Spacing)
     offset_ws_name = ws_name+"offset"+index
     mask_ws_name = ws_name+"mask"+index
-    GetDetectorOffsets(InputWorkspace=ws_name+"cc"+index,
+    GetDetectorOffsets(InputWorkspace=cc_ws_name,
                        OutputWorkspace=offset_ws_name, MaskWorkspace=mask_ws_name,
                        Step=abs(binning),
                        DReference=peak_position,
@@ -54,7 +56,7 @@ def cc_calibrate(ws_name, peak_position, peak_min, peak_max, ws_index_range, ref
                        MaxOffset=max_offset)
 
     # check result and remove interval result
-    if mtd.doesExist(ws_name+"cc"):
+    if False and mtd.doesExist(ws_name+"cc"+index):
         mtd.remove(ws_name+"cc")
 
     return offset_ws_name, mask_ws_name
@@ -74,9 +76,9 @@ def save_calibration(ws_name, offset_mask_list, group_ws_name, calib_file_prefix
     offset_ws_name = ws_name + '_offset'
     mask_ws_name = ws_name + '_mask'
     if offset_ws_name != offset_ws_name0:
-        RenameWorspace(InputWorkspace=offset_ws_name0, OutputWorkspace=offset_ws_name)
+        RenameWorkspace(InputWorkspace=offset_ws_name0, OutputWorkspace=offset_ws_name)
     if mask_ws_name != mask_ws_name0:
-        RenameWorspace(InputWorkspace=mask_ws_name0, OutputWorkspace=mask_ws_name)
+        RenameWorkspace(InputWorkspace=mask_ws_name0, OutputWorkspace=mask_ws_name)
 
     for ituple in range(1, len(offset_mask_list)):
         offset_ws_name_i, mask_ws_name_i = offset_mask_list[ituple]
@@ -88,7 +90,7 @@ def save_calibration(ws_name, offset_mask_list, group_ws_name, calib_file_prefix
     # for the sake of legacy
     SaveCalFile(OffsetsWorkspace=offset_ws_name,
                 GroupingWorkspace=group_ws_name,
-                MaskWorkspace=wkspName+"mask",
+                MaskWorkspace=mask_ws_name,
                 Filename=os.path.join(os.getcwd(), calib_file_prefix + '.cal'))
 
     # the real version
@@ -108,151 +110,110 @@ def save_calibration(ws_name, offset_mask_list, group_ws_name, calib_file_prefix
     return calib_ws_name, offset_ws_name, mask_ws_name
 
 
-def cross_correlate_vulcan_data(group_ws_name):
-    wkspName = 'full_diamond'
+def cross_correlate_vulcan_data(wkspName, group_ws_name):
+    """
+    cross correlation on vulcan data
+    :param wkspName:
+    :param group_ws_name:
+    :return:
+    """
+    # wkspName = 'full_diamond'
     peakpos1 = 1.2614
     peakpos2 = 1.2614
     peakpos3 = 1.07577
 
     ref_ws_index = 1613
-    west_offset, west_mask = cc_calibrate(wkspName, peakpos1, peakpos1-0.01, peakpos1+0.01, [0, 3234],
-                                          ref_ws_index, 100, 1, -0.0003, 'west')
+    peak_width = 0.04   # modified from 0.005
+    cc_number = 80
+    west_offset, west_mask = cc_calibrate(wkspName, peakpos1, peakpos1-peak_width, peakpos1+peak_width, [0, 3234-1],
+                                          ref_ws_index, cc_number, 1, -0.0003, 'west')
 
     ref_ws_index = 4847
-    east_offset, east_mask = cc_calibrate(wkspName, peakpos2, peakpos2-0.01, peakpos2+0.01, [3234, 6468],
-                                          ref_ws_index, 100, 1, -0.0003, 'east')
+    peak_width = 0.04
+    cc_number = 80
+    east_offset, east_mask = cc_calibrate(wkspName, peakpos2, peakpos2-peak_width, peakpos2+peak_width, [3234, 6468-1],
+                                          ref_ws_index, 80, 1, -0.0003, 'east')
+                                          
+    ref_ws_index = 15555
+    peak_width = 0.01
+    cc_number = 20
+    ha_offset, ha_mask = cc_calibrate(wkspName, peakpos3, peakpos3-peak_width, peakpos3+peak_width, [6468, 24900-1],
+                                          ref_ws_index, cc_number, 1, -0.0003, 'high_angle')
 
-    save_calibration(wkspName, [(west_offset, west_mask), (east_offset, east_mask)], group_ws_name, 'vulcan_vz_test')
+    save_calibration(wkspName, [(west_offset, west_mask), (east_offset, east_mask), (ha_offset, ha_mask)], group_ws_name, 'vulcan_vz_test')
 
     return
 
 
+def peak_function(vec_x, function_type):
+    """
 
-def cccalibrate(wksp, peakpos1, peakpos2, peakpos3,  lastpixel=0, ccnumber=100, maxoffset=1):
-    if wksp is None:
-        return None
+    :param vec_x:
+    :param function_type:
+    :return:
+    """
+    # gaussian:
+    vec_y = peak_inensity * numpy.exp(-(vec_x - peak_center)**2/peak_sigma**2) + bkgd_a0 + bkgd_a1 * vec_x
 
-    # Bin events in d-Spacing
-    # Rebin(InputWorkspace=wksp, OutputWorkspace=wksp,
-    #       Params=str(self._peakmin)+","+str(abs(self._binning[1]))+","+str(self._peakmax))
+    return vec_y
 
-    #Find good peak for reference: strongest???
-    ymax = 0
-    for s in range(0,mtd[wksp].getNumberHistograms()):
-        y_s = mtd[wksp].readY(s)
-        midBin = int(mtd[wksp].blocksize()/2)
-        if y_s[midBin] > ymax:
-            refpixel = s
-            ymax = y_s[midBin]
-    # self.log().information("Reference spectra=%s" % refpixel)
-    print ('Reference spectra=%s' % refpixel)
 
-    # Cross correlate spectra using interval around peak at peakpos (d-Spacing)
+def evaluate_cc_quality(data_ws_name, fit_param_table_name):
+    """
 
-    if lastpixel == 0:
-        lastpixel = mtd[wksp].getNumberHistograms()-1
+    :param data_ws_name:
+    :param fit_param_table_name:
+    :return:
+    """
+    data_ws = AnalysisDataService.retrieve(data_ws_name)
+    param_table_ws = AnalysisDataService.retrieve(fit_param_table_name)
+
+    cost_list = list()
+
+    for row_index in range(param_table_ws.rowCount()):
+
+        ws_index = param_table_ws.cell(row_index, 0)
+        peak_pos = param_table_ws.cell(row_index, 1)
+        peak_height = param_table_ws.cell(row_index, 2)
+        peak_sigma = param_table_ws.cell(row_index, 3)
+        bkgd_a0 = param_table_ws.cell(row_index, 4)
+        bkgd_a1 = param_table_ws.cell(row_index, 5)
+
+        peak_fwhm = peak_sigma * whatever
+
+        x_min = peak_pos - 0.5 * peak_fwhm
+        x_max = peak_pos + 0.5 * peak_fwhm
+
+        vec_x = data_ws.readX(ws_index)
+        i_min = bisect.bisect(vec_x, x_min)
+        i_max = bisect.bisect(vec_x, x_max)
+
+        vec_x = vec_x[i_min:i_max]
+        obs_y = data_ws.readY(ws_index)[i_min:i_max]
+        model_y = peak_function(vec_x)
+        cost = numpy.sqrt(sumpy.sum((model_y - obs_y)**2))/len(obs_y)
+
+        cost_list.append([ws_index, cost])
+    # END-FOR
+
+    return cost_list
+
+
+def main(argv):
+    """
+
+    :param argv:
+    :return:
+    """
+    if len(argv) == 0:
+        # default
+        nxs_file_name = 'VULCAN_150178_HighResolution_Diamond.nxs'
     else:
-        lastpixel = int(mtd[wksp].getNumberHistograms()*lastpixel/self._lastpixel3) - 1
-    print ("Last pixel=%s" % lastpixel)
-    CrossCorrelate(InputWorkspace=wksp, OutputWorkspace=wksp+"cc",
-                   ReferenceSpectra=refpixel, WorkspaceIndexMin=0,
-                   WorkspaceIndexMax=lastpixel,
-                   XMin=peakpos1 - 0.01, XMax=peakpos1 + 0.01)
-    # Get offsets for pixels using interval around cross correlations center and peak at peakpos (d-Spacing)
-    GetDetectorOffsets(InputWorkspace=wksp+"cc", OutputWorkspace=wksp+"offset",
-                       Step=0.0003, DReference=peakpos1,
-                       XMin=-ccnumber, XMax=ccnumber,
-                       MaxOffset=maxoffset, MaskWorkspace=wksp+"mask")
-    if AnalysisDataService.doesExist(wksp+"cc"):
-        AnalysisDataService.remove(wksp+"cc")
-    if peakpos2 > 0.0:
-        Rebin(InputWorkspace=wksp, OutputWorkspace=wksp,
-              Params=str(self._peakmin2)+","+str(abs(self._binning[1]))+","+str(self._peakmax2))
-        #Find good peak for reference
-        ymax = 0
-        for s in range(0,mtd[wksp].getNumberHistograms()):
-            y_s = mtd[wksp].readY(s)
-            midBin = int(mtd[wksp].blocksize()/2)
-            if y_s[midBin] > ymax:
-                refpixel = s
-                ymax = y_s[midBin]
-        msg = "Reference spectra = %s, lastpixel_3 = %s" % (refpixel, self._lastpixel3)
-        self.log().information(msg)
-        self._lastpixel2 = int(mtd[wksp].getNumberHistograms()*self._lastpixel2/self._lastpixel3) - 1
-        CrossCorrelate(InputWorkspace=wksp, OutputWorkspace=wksp+"cc2",
-                       ReferenceSpectra=refpixel, WorkspaceIndexMin=self._lastpixel+1,
-                       WorkspaceIndexMax=self._lastpixel2,
-                       XMin=self._peakmin2, XMax=self._peakmax2)
-        # Get offsets for pixels using interval around cross correlations center and peak at peakpos (d-Spacing)
-        GetDetectorOffsets(InputWorkspace=wksp+"cc2", OutputWorkspace=wksp+"offset2",
-                           Step=abs(self._binning[1]), DReference=self._peakpos2,
-                           XMin=ccnumber, XMax=ccnumber,
-                           MaxOffset=self._maxoffset, MaskWorkspace=wksp+"mask2")
-        Plus(LHSWorkspace=wksp+"offset", RHSWorkspace=wksp+"offset2",
-             OutputWorkspace=wksp+"offset")
-        Plus(LHSWorkspace=wksp+"mask", RHSWorkspace=wksp+"mask2",
-             OutputWorkspace=wksp+"mask")
-        for ws in [wksp+"cc2", wksp+"offset2", wksp+"mask2"]:
-            if AnalysisDataService.doesExist(ws):
-                AnalysisDataService.remove(ws)
+        # user specified
+        nxs_file_name = argv[0]
 
-    if peakpos3 > 0.0:
-        Rebin(InputWorkspace=wksp, OutputWorkspace=wksp,
-              Params=str(self._peakmin3)+","+str(abs(self._binning[1]))+","+str(self._peakmax3))
-        #Find good peak for reference
-        ymax = 0
-        for s in range(0,mtd[wksp].getNumberHistograms()):
-            y_s = mtd[wksp].readY(s)
-            midBin = mtd[wksp].blocksize()/2
-            if y_s[midBin] > ymax:
-                refpixel = s
-                ymax = y_s[midBin]
-        self.log().information("Reference spectra=%s" % refpixel)
-        CrossCorrelate(InputWorkspace=wksp, OutputWorkspace=wksp+"cc3",
-                       ReferenceSpectra=refpixel,
-                       WorkspaceIndexMin=self._lastpixel2+1,
-                       WorkspaceIndexMax=mtd[wksp].getNumberHistograms()-1,
-                       XMin=self._peakmin3, XMax=self._peakmax3)
-        # Get offsets for pixels using interval around cross correlations center and peak at peakpos (d-Spacing)
-        GetDetectorOffsets(InputWorkspace=wksp+"cc3", OutputWorkspace=wksp+"offset3",
-                           Step=abs(self._binning[1]), DReference=self._peakpos3,
-                           XMin=-self._ccnumber, XMax=self._ccnumber,
-                           MaxOffset=self._maxoffset, MaskWorkspace=wksp+"mask3")
-        Plus(LHSWorkspace=wksp+"offset", RHSWorkspace=wksp+"offset3",
-             OutputWorkspace=str(wksp)+"offset")
-        Plus(LHSWorkspace=wksp+"mask", RHSWorkspace=wksp+"mask3",
-             OutputWorkspace=wksp+"mask")
-        for ws in [wksp+"cc3", wksp+"offset3", wksp+"mask3"]:
-            if AnalysisDataService.doesExist(ws):
-                AnalysisDataService.remove(ws)
-
-    return str(wksp)
-
-
-def saveCalibration(wkspName, calibFilePrefix):
-    outfilename = None
-
-    # for the sake of legacy
-    SaveCalFile(OffsetsWorkspace=wkspName+"offset",
-                #GroupingWorkspace=wkspName+"group",
-                MaskWorkspace=wkspName+"mask",Filename=calibFilePrefix + '.cal')
-    # the real version
-    outfilename = calibFilePrefix + '.h5'
-    if os.path.exists(outfilename):
-        os.unlink(outfilename)
-    ConvertDiffCal(OffsetsWorkspace=wkspName+"offset",
-                   OutputWorkspace=wkspName+"cal")
-    SaveDiffCal(CalibrationWorkspace=wkspName+"cal",
-                #GroupingWorkspace=wkspName+"group",
-                MaskWorkspace=wkspName+"mask",
-                Filename=outfilename)
-
-    return
-
-# wkspName ='full_diamon'
-# peakpos1 = 1.2614
-# peakpos2 = 1.2614
-# peakpos3 = 1.07577
-# cal_ws_name = cccalibrate(wkspName, peakpos1, peakpos2, peakpos3)
-# print ('Returned workspace: {0}'.format(cal_ws_name))
-# saveCalibration(wkspName, calibFilePrefix='VulcanWestTemp')
+    # decide to load or not and thus group workspace
+    if AnalysisDataService.doesExist('vulcan_diamond') is False:
+        diamond_ws = Load(Filename=nxs_file_name, OutputWorkspace='vulcan_diamond')
+        CreateGroupingWorkspace(InputWorkspace='vulcan_diamond', OutputWorkspace='vulcan_group')
+        cross_correlate_vulcan_data(diamond_ws.name(), 'vulcan_group')
