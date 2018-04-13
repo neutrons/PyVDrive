ede6dd8975c50ed0d8e7fbec126068c4d20b7e2d
Wenduo Zhou <zhouw@ornl.gov>
2018-04-13 00:31:41 -0400

    Refs #99. Improved some testing script for calibration result analysis.

--- scripts/calibration/calculate_difc.py
+++ scripts/calibration/calculate_difc.py
@@ -1,5 +1,8 @@
 import h5py
+import numpy
 import math
+from mantid.api import AnalysisDataService as mtd
+from mantid.simpleapi import CreateWorkspace
 
 
 def get_idf_difc():
@@ -34,19 +37,42 @@ def get_idf_difc():
     out_file = open('raw_difc.dat', 'w')
     out_file.write(out)
     out_file.close()
-   
+
+
 def get_calibrated_difc(cal_ws_name):
     """ go over calibration (table) workspace
     """
     cal_table_ws = mtd[cal_ws_name]
 
     # west bank
+    west_spec_vec = numpy.arange(0, 3234)
+    west_difc_vec = numpy.ndarray(shape=(3234,), dtype='float')
+    for irow in range(0, 3234):
+        west_difc_vec[irow] = cal_table_ws.cell(irow, difc_col_index)
+
     CreateWorkspace(DataX=west_spec_vec, DataY=west_difc_vec, NSpec=1, OutputWorkspace='west_difc')
 
     # east bank
+    east_spec_vec = numpy.arange(3234, 6468)
+    east_difc_vec = numpy.ndarray(shape=(3234,), dtype='float')
+    for irow in range(3234, 6468):
+        west_difc_vec[irow-3234] = cal_table_ws.cell(irow, difc_col_index)
+
     CreateWorkspace(DataX=east_spec_vec, DataY=east_difc_vec, NSpec=1, OutputWorkspace='eastt_difc')
 
     # high angle bank
+    highangle_spec_vec = numpy.arange(6468, 49000)
+    highangle_difc_vec = numpy.ndarray(shape=(3234,), dtype='float')
+    for irow in range(6468, 49000):
+        west_difc_vec[irow-6468] = cal_table_ws.cell(irow, difc_col_index)
+
     CreateWorkspace(DataX=highangle_spec_vec, DataY=highangle_difc_vec, NSpec=1, OutputWorkspace='hig_angle_difc')
 
+    return
 
+
+if 'diamond' not in mtd:
+    Load(Filename='/SNS/VULCAN/IPTS-19576/nexus/VULCAN_160457.nxs.h5', OutputWorkspace='diamond')
+LoadDiffCal(InputWorkspace='diamond',
+            Filename='/home/wzz/Vulcan-Calibration/2017_8_11_CAL/VULCAN_calibrate_2017_08_17_27bank.h5',
+            WorkspaceName='vulcan_27')
--- scripts/calibration/find_unaligned_peaks.py
+++ scripts/calibration/find_unaligned_peaks.py
@@ -1,56 +1,74 @@
-# Clone group
-CloneWorkspace(InputWorkspace='vulcan_27_group', OutputWorkspace='vulcan_debug_group')
+# Goal: Find peaks are not aligned with others in focused TOF and dSpacing
+import mantid
+from mantid.simpleapi import CloneWorkspace, Load, ConvertUnits, AlignAndFocusPowder, LoadDiffCal, Rebin
+from mantid.api import AnalysisDataService as mtd
 
-# Get range of wd index for group4
-group_ws = mtd['vulcan_debug_group']
+# Clone group workspace
+if 'diamond' not in mtd:
+    Load(Filename='/SNS/VULCAN/IPTS-19576/nexus/VULCAN_160457.nxs.h5', OutputWorkspace='diamond')
+LoadDiffCal(InputWorkspace='diamond',
+            Filename='/home/wzz/Vulcan-Calibration/2017_8_11_CAL/VULCAN_calibrate_2017_08_17_27bank.h5',
+            WorkspaceName='vulcan_27')
+group_ws = CloneWorkspace(InputWorkspace='vulcan_27_group', OutputWorkspace='vulcan_debug_group')
+
+# original 27 banks focused data
+vulcan_l1 = 40.
+vulcan_l2_list = [2.] * 27
+vulcan_2theta_list = [270.]*9
+vulcan_2theta_list.extend([90.]*9)
+vulcan_2theta_list.extend([150.]*9)
+vulcan_azimulth_list = [0] * 27
+
+# Get range of wd index for group=4
 ws_index_list = list()
 for iws in range(group_ws.getNumberHistograms()):  # : if 3.9 < group_ws.readX(iws)[0] < 4.1]
     print group_ws.readX(iws)[0]
-    if 3.9 < group_ws.readY(iws)[0] and group_ws.readY(iws)[0] < 4.1:
+    if 3.9 < group_ws.readY(iws)[0] < 4.1:
         ws_index_list.append(iws)
 
 ws_index_list.sort()
 print ws_index_list[0], ws_index_list[-1]
+group4_start_index = ws_index_list[0]
+group4_stop_index = ws_index_list[-1]  # included
 
 # Nail down
 midindex = (1436 + 1078) / 2
 
-""" Step 1  Nail down to lower
+""" Step 1  Nail down to lower """
 for iws in range(1078, 1200):
     group_ws.dataY(iws)[0] = 28
 for iws in range(1200, midindex):
     group_ws.dataY(iws)[0] = 29
-"""
+vulcan_l2_list.extend([2., 2.])
+vulcan_2theta_list.extend([270., 270.])
+vulcan_azimulth_list.extend([0., 0])
+
+spectra_list = range(1, 29)
+
 # step2 : more aggressive
-for iws in range(1078, 1085):
-    group_ws.dataY(iws)[0] = 28
-for iws in range(1085, 1092):
-    group_ws.dataY(iws)[0] = 29
-for iws in range(1092, 1099):
-    group_ws.dataY(iws)[0] = 30
-for iws in range(1099, 2006):
-    group_ws.dataY(iws)[0] = 31
-for iws in range(2006, midindex):
-    group_ws.dataY(iws)[0] = 32
+# for iws in range(1078, midindex):
+#     group_ws.dataY(iws)[0] = 28
+#     
+#     
+#     
+# for iws in range(1085, 1092):
+#     group_ws.dataY(iws)[0] = 29
+# for iws in range(1092, 1099):
+#     group_ws.dataY(iws)[0] = 30
+# for iws in range(1099, 2006):
+#     group_ws.dataY(iws)[0] = 31
+# for iws in range(2006, ):
+#     group_ws.dataY(iws)[0] = 32
 
-
-
-#-------------------------------------------
-Load(Filename='/SNS/VULCAN/IPTS-19576/nexus/VULCAN_160457.nxs.h5', OutputWorkspace='diamond')
+# apply the new group workspace to diamond data
+AlignAndFocusPowder(InputWorkspace='raw_diamond', OutputWorkspace='focus2',
+                    GroupingWorkspace='vulcan_debug_group', CalibrationWorkspace='vulcan_27_cal',
+                    MaskWorkspace='vulcan_27_mask', Params='-0.001', DMin='0.5', DMax='5',
+                    Spectra=spec_list,
+                    L1=vulcan_l1,
+                    L2=vulcan_l2_list,
+                    twotheta=vulcan_2theta_list,
+                    azimuthal=vulcan_azimulth_list)
 ConvertUnits(InputWorkspace='diamond', OutputWorkspace='diamond', Target='dSpacing', ConvertFromPointData=False)
 Rebin(InputWorkspace='diamond', OutputWorkspace='diamond', Params='0.5,-0.001,2', FullBinsOnly=True)
-LoadDiffCal(InputWorkspace='diamond', Filename='/home/wzz/Vulcan-Calibration/2017_8_11_CAL/VULCAN_calibrate_2017_08_17_27bank.h5', WorkspaceName='vulcan_27')
-Load(Filename='/SNS/VULCAN/IPTS-19576/nexus/VULCAN_160457.nxs.h5', OutputWorkspace='raw_diamond')
-AlignAndFocusPowder(InputWorkspace='raw_diamond', OutputWorkspace='focus', GroupingWorkspace='vulcan_27_group', CalibrationWorkspace='vulcan_27_cal', MaskWorkspace='vulcan_27_mask', Params='-0.001', DMin='0.5', DMax='5')
-CloneWorkspace(InputWorkspace='vulcan_27_group', OutputWorkspace='vulcan_debug_group')
-AlignAndFocusPowder(InputWorkspace='raw_diamond', OutputWorkspace='focus2', GroupingWorkspace='vulcan_debug_group', CalibrationWorkspace='vulcan_27_cal', MaskWorkspace='vulcan_27_mask', Params='-0.001', DMin='0.5', DMax='5')
-CloneWorkspace(InputWorkspace='vulcan_27_group', OutputWorkspace='vulcan_debug_group')
-AlignAndFocusPowder(InputWorkspace='raw_diamond', OutputWorkspace='focus2', GroupingWorkspace='vulcan_debug_group', CalibrationWorkspace='vulcan_27_cal', MaskWorkspace='vulcan_27_mask', Params='-0.001', DMin='0.5', DMax='5')
-CloneWorkspace(InputWorkspace='vulcan_27_group', OutputWorkspace='vulcan_debug_group')
-AlignAndFocusPowder(InputWorkspace='raw_diamond', OutputWorkspace='focus2', GroupingWorkspace='vulcan_debug_group', CalibrationWorkspace='vulcan_27_cal', MaskWorkspace='vulcan_27_mask', Params='-0.001', DMin='0.5', DMax='5')
-CloneWorkspace(InputWorkspace='vulcan_27_group', OutputWorkspace='vulcan_debug_group')
-AlignAndFocusPowder(InputWorkspace='raw_diamond', OutputWorkspace='focus2', GroupingWorkspace='vulcan_debug_group', CalibrationWorkspace='vulcan_27_cal', MaskWorkspace='vulcan_27_mask', Params='-0.001', DMin='0.5', DMax='5')
-CloneWorkspace(InputWorkspace='vulcan_27_group', OutputWorkspace='vulcan_debug_group')
-AlignAndFocusPowder(InputWorkspace='raw_diamond', OutputWorkspace='focus2', GroupingWorkspace='vulcan_debug_group', CalibrationWorkspace='vulcan_27_cal', MaskWorkspace='vulcan_27_mask', Params='-0.001', DMin='0.5', DMax='5')
-CloneWorkspace(InputWorkspace='vulcan_27_group', OutputWorkspace='vulcan_debug_group')
-AlignAndFocusPowder(InputWorkspace='raw_diamond', OutputWorkspace='focus2', GroupingWorkspace='vulcan_debug_group', CalibrationWorkspace='vulcan_27_cal', MaskWorkspace='vulcan_27_mask', Params='-0.001', DMin='0.5', DMax='5')
+
