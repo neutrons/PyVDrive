91e7c288bf575abbff4a4d7e4bbc24e88f95349d
Wenduo Zhou <zhouw@ornl.gov>
2018-05-15 22:19:13 -0400

    Refs #105. Improved code quality.

--- pyvdrive/lib/file_utilities.py
+++ pyvdrive/lib/file_utilities.py
@@ -2,7 +2,7 @@
 # All the PyVDrive specific files will be parsed or written by methods in this module
 import os
 import h5py
-import vdrivehelper
+import datatypeutility
 from mantid.simpleapi import SaveNexusProcessed, LoadNexusProcessed
 
 
@@ -26,16 +26,46 @@ def import_detector_efficiency(h5_name):
 
     return pid_vec, det_eff_factor_vec
 
-def load_processed_nexus():
-    # TODO
-    LoadNexusProcessed(Filename='/home/wzz/Projects/PyVDrive/tests/data/vulcan_vanadium.nxs',
-                       OutputWorkspace='7bankvanadium')
+
+def load_processed_nexus(nexus_file_name, output_ws_name):
+    """
+    load a Mantid processed Nexus file
+    :param nexus_file_name:
+    :param output_ws_name:
+    :return:
+    """
+    datatypeutility.check_file_name(nexus_file_name, check_exist=True, check_writable=False,
+                                    note='Mantid processed NeXus file')
+    datatypeutility.check_string_variable('Output workspace name', output_ws_name)
+    # LoadNexusProcessed(Filename='/home/wzz/Projects/PyVDrive/tests/data/vulcan_vanadium.nxs',
+    #                    OutputWorkspace='7bankvanadium')
+
+    LoadNexusProcessed(Filename=nexus_file_name, OutputWorkspace=output_ws_name)
 
     return
 
+
 def save_workspace(ws_name, file_name, file_type='nxs', title=''):
+    """
+    save a workspace to Mantid processed Nexus file
+    :param ws_name:
+    :param file_name:
+    :param file_type:
+    :param title:
+    :return:
+    """
+    datatypeutility.check_string_variable('Workspace name', ws_name)
+    datatypeutility.check_file_name(file_name=file_name, check_writable=True,
+                                    check_exist=False,
+                                    note='Output processed NeXus file')
+    datatypeutility.check_string_variable('Workspace title', title)
+
     if (file_type is None and file_name.lower().ends('.nxs')) or file_type == 'nxs':
         SaveNexusProcessed(InputWorkspace=ws_name,
                            Filename=file_name,
                            Title=title)
+    else:
+        raise RuntimeError('File type {0} or output file postfix {1} is not supported for saving.'
+                           ''.format(file_type, file_name.lower()))
 
+    return
