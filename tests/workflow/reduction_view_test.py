ba04741fe3bf5a96e58ca9b052b8620761438355
Wenduo Zhou <zhouw@ornl.gov>
2018-05-22 15:19:16 -0400

    Rfs #105. Added a test script.

--- tests/workflow/reduction_view_test.py
+++ tests/workflow/reduction_view_test.py
@@ -7,6 +7,9 @@ try:
 except ImportError:
     print ('PyQt4 will be imported')
     from PyQt4.QtGui import QApplication
+mac_mantid_path = '/Users/wzz/MantidBuild/debug/bin/'
+sys.path.append(mac_mantid_path)
+import mantid
 from pyvdrive.interface.VDrivePlot import VdriveMainWindow
 
 
