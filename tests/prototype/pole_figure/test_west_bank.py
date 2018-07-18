c8b1dc31da4af736fae2eed6ced8644e8e1d1f5e
Wenduo Zhou <zhouw@ornl.gov>
2018-07-18 14:54:15 -0400

    Refs #115. Added some files for pole figure algorithm's test.

---
+++ tests/prototype/pole_figure/test_west_bank.py
@@ -0,0 +1,3 @@
+CreateWorkspace(OutputWorkspace='vulcan_west_bank', DataX='1,2,1,2,1,2,1,2,1,2', DataY='2,2,2,2,2', DataE='1,1,1,1,1', NSpec=5, UnitX='dSpacing')
+EditInstrumentGeometry(Workspace='vulcan_west_bank', PrimaryFlightPath=-43.753999999999998, SpectrumIDs='1-5', L2='2.04302,2.04302,2,2.04302,2.04302', Polar='100.791,100.791,90,79.2093,79.2093', Azimuthal='-4.75179,4.75179,0,-4.75179,4.75179', DetectorIDs='1-5', InstrumentName='VulcanWestBankOnly')
+
