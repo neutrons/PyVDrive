8f3f0bb5f0cd8cd58a9ddf14e31d1ca4e950c44b
Wenduo Zhou <zhouw@ornl.gov>
2018-07-20 08:07:48 -0400

    Refs #115. Checkpointing progress on pole figure prototype.

--- tests/prototype/pole_figure/test_west_bank.py
+++ tests/prototype/pole_figure/test_west_bank.py
@@ -23,3 +23,67 @@ log_setup_file = open('vulcan_sample_log.txt', 'r')
 lines = log_setup_file.readlines()
 log_setup_file.close()
 
+hrot_omega_list = list()
+for line in lines:
+    line = line.strip()
+    if len(line) == 0:
+        continue
+    elif line.startswith('#'):
+        continue
+
+    str_list = line.split()
+    hrot = float(str_list[0])
+    omega = float(str_list[1])
+
+    hrot_omega_list.append((hrot, omega))
+# END-FOR
+
+# import pole figure
+pole_figure_file = open('vulcan_sample_log.txt', 'r')
+lines = log_setup_file.readlines()
+log_setup_file.close()
+
+pole_figure_list = list()
+for line in lines:
+    line = line.strip()
+    if len(line) == 0:
+        continue
+    elif line.startswith('#'):
+        continue
+
+    str_list = line.split()
+    r_td_list = list()
+    r_nd_list = list()
+    for i in range(5):
+        r_td_list.append(float(str_list[2*i]))
+        r_nd_list.append(float(str_list[2*i+1]))
+    # END-FOR
+
+    pole_figure_list.append((r_td_list, r_nd_list))
+# END-FOR
+
+# check against Mantid's algorithm
+if len(hrot_omega_list) != len(pole_figure_list):
+    raise RuntimeError('HROT/OMEGA list has a different size other than pole figure value list.  They are '
+                       'not in 1-to-1 mapping')
+
+num_tests = len(hrot_omega_list)
+
+for itest in range(num_tests):
+    # set up the instrument (test purpose)
+    simple.AddSampleLog(Workspace='vulcan_west_bank', LogName='hrot_{0}'.format(itest),
+                        LogText='{}'.format(hrot_omega_list[itest][0]),
+                        LogType='Number Series')
+    simple.AddSampleLog(Workspace='vulcan_west_bank', LogName='omega_{0}'.format(itest),
+                        LogText='{}'.format(hrot_omega_list[itest][1]),
+                        LogType='Number Series')
+
+    # integration
+    simple.Integration(InputWorkspace='vulcan_west_bank', OutputWorkspace='peak_intensity',
+                       RangeLower=1.2, RangeUpper=1.8)
+
+    # do conversion
+    simple.ConvertToPoleFigure()
+
+    # compare value
+# END-FOR
