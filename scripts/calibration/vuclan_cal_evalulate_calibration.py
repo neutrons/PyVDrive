46d972c37f0f0135f71ccc40624006fb4ac72e43
Wenduo Zhou <zhouw@ornl.gov>
2018-09-12 07:08:17 -0400

    Refs #121. Checkpointing progress on cleaning up the calibration and analysis script.

--- scripts/calibration/vuclan_cal_evalulate_calibration.py
+++ scripts/calibration/vuclan_cal_evalulate_calibration.py
@@ -1,23 +1,12 @@
+#!/bin/python
 # It is to evaluate the calibration result
+import sys
+import os
+from mantid.simpleapi import Load, LoadDiffCal
+
+import lib_cross_correlation as lib
 
 
-def load_raw_nexus(ipts_number, run_number, calib_file_name):
-    """
-    reduced: aligned detector and diffraction focus, powder data
-    :param ipts_number:
-    :param run_number:
-    :return:
-    """
-    raw_nxs_file_name = '/SNS/VULCAN/IPTS-{}/nexus/VULCAN_{}.nxs.h5'.format(ipts_number, run_number)
-    Load(Filename=raw_nxs_file_name, OutputWorkspace='vulcan_diamond')
-
-    return
-
-def load_calibration_file():
-
-    # load data file
-    LoadDiffCal(InputWorkspace='vulcan_diamond',
-                Filename=calib_file_name, WorkspaceName='vulcan')
 
 
 
@@ -49,3 +38,83 @@ def diffraction_focus():
                            Polar='89.9284,90.0716,150.059', Azimuthal='0,0,0', DetectorIDs='1-3',
                            InstrumentName='vulcan_3bank')
 
+
+def parse_inputs(argv):
+    """
+    parse inputs
+    :param argv:
+    :return:
+    """
+    arg_dict = dict()
+
+    # go through each term
+    sub_arg_list = argv.split()
+    for sub_arg in sub_arg_list:
+        items = sub_arg.split('=')
+        if items[0] == '--ipts':
+            arg_dict['ipts'] = int(items[1])
+        elif items[0] == '--run':
+            arg_dict['run'] = int(items[1])
+        elif items[0] == '--nexus':
+            arg_dict['event_nexus'] = str(items[1])
+        elif items[0] == '--calib':
+            arg_dict['calib_file'] = str(items[1])
+        else:
+            print ('Argument {} is not supported'.format(items[0]))
+    # END-FOR
+
+    return arg_dict
+
+
+def print_help(script_name):
+    """
+    print out help information
+    :return:
+    """
+    print ('Format: {} --ipts=xxx --run=xxx --nexus=blabla.nxs --calib=balbal.h5'.format(script_name))
+
+    return
+
+
+def main(argv):
+    """
+    main method
+    :param argv:
+    :return:
+    """
+    if len(argv) == 1:
+        print ('Help: {} --help'.format(argv[0]))
+        sys.exit(0)
+
+    # help
+    if '--help' in argv:
+        print_help(script_name=argv[0])
+        sys.exit(1)
+
+    # parse input arguments
+    input_arg_dict = parse_inputs(argv[1:])
+
+    # load event data
+    if 'event_nexus' in input_arg_dict:
+        event_ws = load_raw_nexus(file_name=input_arg_dict['event_nexus'], ipts=None, run_number=None)
+    else:
+        ipts_number = input_arg_dict['ipts']
+        run_number = input_arg_dict['run']
+        event_ws = load_raw_nexus(file_name=None, ipts=ipts_number, run_number=run_number)
+    # END-IF
+
+    # load calibration file
+    calib_ws, mask_ws, group_ws = load_calibration_file(ref_ws=event_ws, diff_cal_file=input_arg_dict['calib_file'])
+
+    # analyze the masking
+    zero_count_mask_ws, low_count_mask_ws, regular_count_mask_ws =\
+        analysize_mask(event_ws, mask_ws, output_dir=os.getcwd())
+
+    # export the
+
+
+
+
+
+if __name__ == '__main__':
+    main(sys.argv)
--- scripts/calibration/vulcan_cal_bank_calibration.py
+++ scripts/calibration/vulcan_cal_bank_calibration.py
@@ -3,135 +3,11 @@ import os
 import sys
 import math
 import numpy
-import cross_correlation_lib as ccl
+import lib_cross_correlation as ccl
 
 # sys.path.insert(1, '/SNS/users/wzz/Mantid_Project/vulcan-build/bin/')
 
 
-# 
-# TODO - 20180911 - move to lib and it is for 3 banks... rename!
-def check_correct_difcs(ws_name):
-    """
-    check and correct DIFCs if necessary: it is for 3 banks
-    :param ws_name:
-    :return:
-    """
-    # west bank
-    ws = mtd['vulcan_diamond']
-    cal_table_ws = mtd['vulcan_diamond_2fit_cal']
-    difc_col_index = 1
-    # west_spec_vec = numpy.arange(0, 3234)
-    west_idf_vec = numpy.ndarray(shape=(3234,), dtype='float')
-    west_cal_vec = numpy.ndarray(shape=(3234,), dtype='float')
-    for irow in range(0, 3234):
-        west_idf_vec[irow] = calcualte_difc(ws, irow)
-        west_cal_vec[irow] = cal_table_ws.cell(irow, difc_col_index)
-    # CreateWorkspace(DataX=west_spec_vec, DataY=west_difc_vec, NSpec=1, OutputWorkspace='west_idf_difc')
-
-    # east bank
-    # east_spec_vec = numpy.arange(3234, 6468)
-    east_idf_vec = numpy.ndarray(shape=(3234,), dtype='float')
-    east_cal_vec = numpy.ndarray(shape=(3234,), dtype='float')
-    for irow in range(3234, 6468):
-        east_idf_vec[irow - 3234] = calcualte_difc(ws, irow)
-        east_cal_vec[irow - 3234] = cal_table_ws.cell(irow, difc_col_index)
-    # CreateWorkspace(DataX=east_spec_vec, DataY=east_difc_vec, NSpec=1, OutputWorkspace='east_idf_difc')
-
-    # high angle bank
-    # highangle_spec_vec = numpy.arange(6468, 24900)
-    highangle_idf_vec = numpy.ndarray(shape=(24900 - 6468,), dtype='float')
-    highangle_cal_vec = numpy.ndarray(shape=(24900 - 6468,), dtype='float')
-    for irow in range(6468, 24900):
-        highangle_idf_vec[irow - 6468] = calcualte_difc(ws, irow)
-        highangle_cal_vec[irow - 6468] = cal_table_ws.cell(irow, difc_col_index)
-
-    mask_ws = mtd['vulcan_diamond_2fit_mask']
-
-    # do correction: west
-    correct_difc_to_default(west_idf_vec, west_cal_vec, cal_table_ws, 0, 20, 1, mask_ws)
-    correct_difc_to_default(east_idf_vec, east_cal_vec, cal_table_ws, 3234, 20, 1, mask_ws)
-    correct_difc_to_default(west_idf_vec, west_cal_vec, cal_table_ws, 6468, 20, 1, mask_ws)
-
-    return
-
-
-# TODO - 20180911 - move to lib and merge with another save_calibration
-def save_calibration(ref_ws_name):
-    """
-
-    :param ref_ws_name:
-    :return:
-    """
-    # Load an existing 3-bank calibration file for grouping
-    exist3bank = '/SNS/users/wzz/Projects/VULCAN/CalibrationInstrument/Calibration_20180530/VULCAN_calibrate_2018_04_12.h5'
-    LoadDiffCal(InputWorkspace=ref_ws_name,
-                Filename=exist3bank,
-            WorkspaceName='vulcan_old_3banks')
-    
-    SaveDiffCal(CalibrationWorkspace='',
-                GroupingWorkspace='vulcan_exist_grouping',
-                MaskWorkspace='')
-
-    return
-
-
-# TODO - 20180911 - move to lib
-def calcualte_difc(ws, ws_index):
-    # det_id = ws.getDetector(i).getID()
-    det_pos = ws.getDetector(ws_index).getPos()
-    source_pos = ws.getInstrument().getSource().getPos()
-    sample_pos = ws.getInstrument().getSample().getPos()
-
-    source_sample = sample_pos - source_pos
-    det_sample = det_pos - sample_pos
-    angle = det_sample.angle(source_sample)
-
-    L1 = source_sample.norm()
-    L2 = det_sample.norm()
-
-    # theta = angle * 180/3.14
-    # print theta
-
-    difc = 252.816 * 2 * math.sin(angle * 0.5) * (L1 + L2)  # math.sqrt(L1+L2) #\sqrt{L1+L2}
-
-    return difc
-
-
-# TODO - 20180911 - move to lib
-def correct_difc_to_default(idf_difc_vec, cal_difc_vec, cal_table, row_shift, difc_tol, difc_col_index, mask_ws):
-    """ Compare the DIFC calculated from the IDF and calibration.
-    If the difference is beyond tolerance, using the IDF-calculated DIFC instead and report verbally
-    :param idf_difc_vec: DIFC calculated from the instrument geometry (engineered)
-    :param cal_difc_vec: DIFC calculated from the calibration
-    :param cal_table: calibration value table (TableWorkspace)
-    :param row_shift: starting row number the first element in DIFC vector
-    :param difc_tol: tolerance on the difference between calculated difc and calibrated difc
-    :param difc_col_index: column index of the DIFC in the table workspace
-    :param mask_ws: mask workspace
-    :return:
-    """
-    # difference between IDF and calibrated DIFC
-    difc_diff_vec = idf_difc_vec - cal_difc_vec
-
-    # go over all the DIFCs
-    num_corrected = 0
-    message = ''
-    for index in range(len(difc_diff_vec)):
-        if abs(difc_diff_vec[index]) > difc_tol:
-            cal_table.setCell(index+row_shift, difc_col_index, idf_difc_vec[index])
-            if mask_ws.readY(index+row_shift)[0] < 0.5:
-                mask_sig = 'No Mask'
-                num_corrected += 1
-            else:
-                mask_sig = 'Masked'
-            message += '{0}: ws-index = {1}, diff = {2}...  {3}\n' \
-                       ''.format(index, index+row_shift, difc_diff_vec[index], mask_sig)
-        # END-IF
-    # END-FOR
-    print (message)
-    print ('Number of corrected DIFC = {0}'.format(num_corrected))
-
-    return
 
 
 def plot_difc_diff(idf_difc_vec, cal_difc_vec, start_index, mask_ws):
@@ -203,6 +79,8 @@ def main(argv):
         dates = sorted(Diamond_Runs.keys())
         nxs_file_name = Diamond_Runs[dates[-1]]
 
+    input_arg_dict = parse_inputs(argv[:1])
+
     # decide to load or not and thus group workspace
     diamond_ws_name, group_ws_name = ccl.initialize_calibration(nxs_file_name, False)
 
@@ -212,10 +90,13 @@ def main(argv):
     # check the difference between DIFCs
     check_correct_difcs(ws_name='vulcan_diamond')
 
-
     # save calibration file
-    save_calibration(ref_ws_name='vulcan_diamond')
+    for num_banks in [3, 7, 27]:
+        export_diff_cal_h5(ref_ws_name='vulcan_diamond', offset_ws=xx, mask_ws=yy, num_groups=num_banks)
 
+    # save difc file
+    if 'difc' in input_arg_dict:
+        ccl.export_difc(offset_ws=xx, file_name=input_arg_dict['difc'])
 
     # plot_difc_diff(mask=mask_ws)
 
