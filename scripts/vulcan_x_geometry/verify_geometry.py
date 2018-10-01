41bbf4cf71577c895fbc198660f585b583aac5d4
Wenduo Zhou <zhouw@ornl.gov>
2018-10-01 17:06:30 -0400

    Refs #117. Tested geometry verification script.

--- scripts/vulcan_x_geometry/verify_geometry.py
+++ scripts/vulcan_x_geometry/verify_geometry.py
@@ -50,12 +50,23 @@ def main(ws_name):
         corner_ws_index_dict['upper right'] = end_ws_index
         corner_ws_index_dict['lower right'] = end_ws_index - num_tube_pixels + 1
 
-        print ('Bank {} Corners:\n'.format(bank_id))
+        print ('Bank {} Corners:'.format(bank_id))
         for corner in sorted(corner_ws_index_dict.keys()):
             det_id = workspace.getDetector(corner_ws_index_dict[corner]).getID()
             det_pos = workspace.getDetector(corner_ws_index_dict[corner]).getPos()
             print ('{}: Detector ID = {}  @ X = {}, Y = {}, Z= {}'
                    ''.format(corner, det_id, det_pos.X(), det_pos.Y(), det_pos.Z()))
+                   
+        # center from corner
+        sum_x = 0
+        sum_y = 0
+        sum_z = 0
+        for corner in sorted(corner_ws_index_dict.keys()):
+            det_pos = workspace.getDetector(corner_ws_index_dict[corner]).getPos()
+            sum_x += det_pos.X()
+            sum_y += det_pos.Y()
+            sum_z += det_pos.Z()
+        print ('\tCenter calculated from corners: X = {}, Y = {}, Z = {}'.format(sum_x*0.25, sum_y*0.25, sum_z*0.25))
 
         # center 4 pixels: general to N*8-pack case, but not N*8 tube case
         center_ws_index_dict = dict()
@@ -66,24 +77,24 @@ def main(ws_name):
 
             # find center
             center_ws_index_dict['upper left'] = \
-                (8 * num_tube_pixels) * left_pack_id + (num_tube_pixels * 7) + num_tube_pixels / 2
+                (8 * num_tube_pixels) * left_pack_id + (num_tube_pixels * 7) + num_tube_pixels / 2 + start_ws_index
             center_ws_index_dict['lower left'] = center_ws_index_dict['upper left'] - 1
 
-            center_ws_index_dict['upper right'] = (8 * num_tube_pixels) * right_pack_id + num_tube_pixels / 2
+            center_ws_index_dict['upper right'] = (8 * num_tube_pixels) * right_pack_id + num_tube_pixels / 2 + start_ws_index
             center_ws_index_dict['lower right'] = center_ws_index_dict['upper right'] - 1
 
         else:
             # odd: pick the middle pack
             middle_pack_id = (num_tubes/8) / 2
-            start_pack_index = middle_pack_id * 8
+            start_pack_tube_index = middle_pack_id * 8
 
             # find center
-            center_ws_index_dict['upper left'] = start_pack_index + 3 * num_tube_pixels + num_tube_pixels / 2
+            center_ws_index_dict['upper left'] =  start_ws_index + (start_pack_tube_index + 3) * num_tube_pixels + num_tube_pixels / 2
             center_ws_index_dict['lower left'] = center_ws_index_dict['upper left'] - 1
             center_ws_index_dict['upper right'] = center_ws_index_dict['upper left'] + num_tube_pixels
             center_ws_index_dict['lower right'] = center_ws_index_dict['lower left'] + num_tube_pixels
         # END-IF
-        print ('Bank {} Center:\n'.format(bank_id))
+        print ('Bank {} Center:'.format(bank_id))
         for center in sorted(center_ws_index_dict.keys()):
             det_id = workspace.getDetector(center_ws_index_dict[center]).getID()
             det_pos = workspace.getDetector(center_ws_index_dict[center]).getPos()
@@ -114,11 +125,11 @@ def main(ws_name):
         for pos_name in corner_ws_index_dict:
             ws_index_i = corner_ws_index_dict[pos_name]
             detector_id_list.append(ws_index_i)
-        MaskDetectorList(InputWorkspace=ws_name, OutputWorkspace=ws_name, DetectorIDList=detector_id_list)
+        MaskDetectors(Workspace=ws_name, WorkspaceIndexList=detector_id_list)
 
         # prepare for the next bank
         start_ws_index = end_ws_index + 1
     # END-FOR
 
 
-main(ws_name='')
+main(ws_name='sim_c_vulcan-x_prototype')
