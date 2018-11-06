4ee9768818f0a7fd09c337487642a3dc0189c833
Wenduo Zhou <zhouw@ornl.gov>
2018-11-06 16:11:33 -0500

    Refs #126. Fotgot this file.

---
+++ analyze_collimator.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+python setup.py pyuic
+python setup.py build
+if [ $1 ]; then
+    CMD=$1
+else
+    CMD=''
+fi
+
+# Core Test
+PYTHONPATH=build/lib:$PYTHONPATH $CMD tests/development/collimator.py
+
+
+# IDL-VDRIVE Commands
+# PYTHONPATH=build/lib:$PYTHONPATH $CMD build/scripts-2.7/vbin_test.py    # Retest
+# PYTHONPATH=build/lib:$PYTHONPATH $CMD build/scripts-2.7/chop_reduce_command_test.py
+# PYTHONPATH=build/lib:$PYTHONPATH $CMD build/scripts-2.7/vbin_test.py
+# PYTHONPATH=build/lib:$PYTHONPATH $CMD build/scripts-2.7/reduction_view_test.py
+# PYTHONPATH=build/lib:$PYTHONPATH $CMD build/scripts-2.7/idl_info_test.py
+# PYTHONPATH=build/lib:$PYTHONPATH $CMD build/scripts-2.7/idl_view_test.py
+
+# GUI Tests
+# PYTHONPATH=build/lib:$PYTHONPATH $CMD build/scripts-2.7/peakfitgui_test.py
+
+# Unit/Concept Tests
+# PYTHONPATH=build/lib:$PYTHONPATH $CMD build/scripts-2.7/vulcan_slice_reduce_test.py
